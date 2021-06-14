5bda4893eb18aaa4166e4b451cb602d143a98fe0
Shannon A <slekupvimplyrataqq@protonmail.com>
2021-06-14 22:36:15 +1200

    Update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-print('running program...')
 import json
 import socket
 h_name = socket.gethostname()
 IP_addres = socket.gethostbyname(h_name)
 from datetime import datetime
 def add_log(text):
+    print(text)
     with open('config.json', 'r') as jsonConfig:
         config = json.load(jsonConfig)
         if config['logOption'] == 'enabled':
@@ -14,11 +14,12 @@ def add_log(text):
                 file_object.write('\n'+dt_string+' - '+h_name+'('+IP_addres+')'+': '+text.lower())
 add_log('running main app')
 
-
+first_time = False
 config_username = 'root'
 with open('config.json', 'r') as jsonConfig:
     config = json.load(jsonConfig)
     if config['username'] == 'root':
+        first_time = True
         print('Welcome, first make a username. Something short and easy to remember! ')
         while config['username'] == 'root':
             username_prompt = input('Enter username: ')
@@ -92,8 +93,6 @@ except:
 try:
     from pynput.mouse import Button, Controller
     mouse = Controller()
-    mouse.position = (0, 0)
-    print(Fore.BLUE+'Your mouse has been moved to the top left.')
     add_log('successfully imported pynput')
 except:
     add_log('error importing pynput')
@@ -102,14 +101,8 @@ except:
     pip.main(['install', 'pynput'])
     #from pynput.keyboard import Key, Controller
     from pynput.mouse import Button, Controller
-    from pynput import mouse
     add_log('installed and imported pynput successfully')
 
-""" This is annoying
-for item in range(1, 100):
-    sleep(0.5)
-    mouse.position = (0, 0)
-"""
 
 # Import keyboard module
 # If the module is not installed, then automatically install it. Otherwise, continue the program
@@ -141,7 +134,6 @@ def check_internet_connection():
 
 def check_internet_on_start():
     if(check_internet_connection() == True):
-        print('connected to the internet')
         add_log('connected to the internet')
     else:
         add_log('not connected to the internet')
@@ -203,16 +195,17 @@ def mod_config(option, value, newval):
         with open('config.json', 'w') as f:
             json.dumps(json.dump(config, f), indent=4)
 
-
-
 def stats():
     return ''
 
 
 # <<<<<<<<<<<<<<<<<<<< <<<<<<<<<< SCRIPT >>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>
-
-
 check_internet_on_start()
+screen_clear()
+mouse.position = (0, 0)
+
+print(Fore.BLUE+'Your mouse has been moved to the top left.')
+
 check_platform_on_start()
 
 
@@ -223,60 +216,120 @@ print(Fore.CYAN+'Your current balance is: '+Fore.RED+'${}'.format(mod_config('vi
 print(Fore.GREEN+'Welcome to the main menu. Use a command or type "help" if you don\'t know any commands')
 
 
+def activate_admin():
+    print('admin activated')
+
+keyboard.add_hotkey("shift+alt+p", lambda: activate_admin)
+
+if first_time == True:
+    from keyboard import press
+    keyboard.write('help')
+
+
+# Function that contains all code for commands, some link to other functions
 def command_line():
     user_input = ''
     while user_input != 'quit':
         with open('config.json', 'r') as jsonConfig:
             config = json.load(jsonConfig)
             config_username = config['username']
-            print(Fore.RED+'\nroot@'+config_username+Fore.WHITE+':'+Fore.BLUE+'~'+Fore.WHITE+'$ ', end='')
+            print(Fore.RED+'\npython-games@'+config_username+Fore.WHITE+':'+Fore.BLUE+'~'+Fore.WHITE+'$ ', end='')
         user_input = input().lower()
       
         if user_input == 'clear':
             screen_clear()
-
-        elif user_input == 'lu':
-            print(Fore.BLUE+'Opening Lucky Unicorn Game...')
-            sleep(1)
-            os.system('python games/luckyunicorn/00_LU_Base_v_01.py')
-
-        elif user_input == 'rps':
-            print(Fore.BLUE+'Opening Rock Paper Scissors Game...')
-            sleep(1)
-            os.system('python games/rock-paper-scissors/app.py')
-
-        elif user_input == 'hl':
-            print('coming soon .... \ngoing back to main menu...')
-
-        elif user_input == 'stats':
-            # creating a DataFrame
-            dict = {'Stats':['Connected to Internet', 'Balance'],
-                    'Result':[
-                        check_internet_connection(),
-                        '${}'.format(mod_config('view','balance',''))
-                    ]}
-            df = pd.DataFrame(dict)
-            # displaying the DataFrame
-            print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
-
-        elif user_input == 'system':
-            print(Fore.BLUE+'Opening python file for "system information"...')
-            os.system('python info/system.py')
-
-        elif user_input == 'profile':
-            print(Fore.BLUE+'Opening profile website')
-            import webbrowser
-            webbrowser.open('https://github.com/shannon-nz')
 
         elif user_input == 'doc':
             print(Fore.BLUE+'Opening python games documentation')
             import webbrowser
             webbrowser.open('https://github.com/shannon-nz/python-games#-python-games-in-development-')
 
-        elif user_input == 'ls':
-            print(Fore.BLUE+'Opening python games documentation')
-            import webbrowser
-            webbrowser.open('https://github.com/shannon-nz/python-games#-python-games-in-development-')
+        elif user_input == 'help':
+            print(Fore.BLACK+'\n   clear           '+Fore.BLUE+'clear the terminal (action)')
+            print(Fore.BLACK+'   doc             '+Fore.BLUE+'open python games documentation (website)')
+            print(Fore.BLACK+'   help            '+Fore.BLUE+'display command details (help)')
+            print(Fore.BLACK+'   help -des       '+Fore.BLUE+'display command details and an longer description (help)')
+            print(Fore.BLACK+'   hl              '+Fore.BLUE+'higher/lower (game)')
+            print(Fore.BLACK+'   log             '+Fore.BLUE+'view all past logs (info)')
+            print(Fore.BLACK+'   log -clean      '+Fore.BLUE+'view all past logs (info)')
+            print(Fore.BLACK+'   log -disable    '+Fore.BLUE+'disable logs (action)')
+            print(Fore.BLACK+'   log -enable     '+Fore.BLUE+'enable logs (info)')
+            print(Fore.BLACK+'   lu              '+Fore.BLUE+'lucky unicorn (game)')
+            print(Fore.BLACK+'   profile         '+Fore.BLUE+'open my profile website (website)')
+            print(Fore.BLACK+'   quit            '+Fore.BLUE+'quit the entire program (action)')
+            print(Fore.BLACK+'   rps             '+Fore.BLUE+'rock paper scissors (game)')
+            print(Fore.BLACK+'   restart         '+Fore.BLUE+'restart the application (action)')
+            print(Fore.BLACK+'   settings        '+Fore.BLUE+'print settings in JSON format (info)')
+            print(Fore.BLACK+'   settings -reset '+Fore.BLUE+'reset back to factory settings (action)')
+            print(Fore.BLACK+'   spaz            '+Fore.BLUE+'SPAZ YOUR SCREEN for 7 seconds (fun)')
+            print(Fore.BLACK+'   stats           '+Fore.BLUE+'View your stats')
+            print(Fore.BLACK+'   system          '+Fore.BLUE+'get your system information (info)')
+
+        elif user_input == 'help -des':
+            print('help -des coming soon')
+
+        elif user_input == 'hl':
+            print('coming soon .... \ngoing back to main menu...')
+            
+        elif user_input == 'log':
+            print(Fore.BLUE+'Printing main.log ...')
+            f = open('main.log', 'r')
+            file_contents = f.read()
+            print (file_contents)
+            f.close()
+
+        elif user_input == 'log -clean':
+            print('cleaning log...')
+            myText = open(r'main.log','w')
+            myText.write('<<< start of log >>>')
+            myText.close()
+            print('log cleaned!')
+
+        elif user_input == 'log -disable':
+            mod_config('mod','logOption','disabled')
+
+        elif user_input == 'log -enable':
+            mod_config('mod','logOption','enabled')
+
+        elif user_input == 'lu':
+            print(Fore.BLUE+'Opening Lucky Unicorn Game...')
+            sleep(1)
+            os.system('python games/luckyunicorn/00_LU_Base_v_01.py')
+
+        elif user_input == 'profile':
+            print(Fore.BLUE+'Opening profile website')
+
+        elif user_input == 'quit':
+            print(Fore.RED+'quiting the application...')
+            exit()
+            print(Fore.RED+'Failed to quit the application, please try again')
+
+        elif user_input == 'restart':
+            os.system('python app.py')
+            exit()
+
+        elif user_input == 'rps':
+            print(Fore.BLUE+'Opening Rock Paper Scissors Game...')
+            sleep(1)
+            os.system('python games/rock-paper-scissors/app.py')
+
+        elif user_input == 'settings':
+            with open('config.json', 'r') as jsonConfig:
+                config = json.dumps(json.load(jsonConfig), sort_keys=True, indent=4, separators=(',', ': '))
+            print(Fore.YELLOW+config)
+            os.system('python app.py')
+            exit()
+
+        elif user_input == 'settings -reset':
+            current_directory = os.path.dirname(__file__)
+            file_path = os.path.join(current_directory, 'main', 'default_config.json')
+            with open(file_path, 'r') as jsonConfig:
+                default_config = json.load(jsonConfig)
+
+            with open('config.json', 'r') as jsonConfig:
+                config = json.load(jsonConfig)
+            with open('config.json', 'w') as f:
+                json.dumps(json.dump(default_config, f), indent=4)
 
         elif user_input == 'spaz':
             print(Fore.RED+'SPAZING YOUR SCREEN for 7 seconds !  DO NOT CLICK ANYTHING')
@@ -297,88 +350,47 @@ def command_line():
                 keyboard.release('windows')
                 keyboard.press('alt+tab')
                 keyboard.release('alt+tab')
-                rand1 = random.randrange(0, 1900)
-                rand2 = random.randrange(0, 1900)
+                rand1 = random.randrange(0, 2000)
+                rand2 = random.randrange(0, 2000)
                 mouse.position = (rand1, rand2)
                 print(Fore.RED+spaz_msg)
                 print(Fore.BLUE+spaz_msg)
-                print(Fore.YELLOW+spaz_msg)
                 print(Fore.WHITE+spaz_msg)
                 print(Fore.BLACK+spaz_msg)
                 print(Fore.GREEN+spaz_msg)
                 print(Fore.MAGENTA+spaz_msg)
-                print(Fore.CYAN+spaz_msg)
                 sleep(0.1)
             print('\n\nAnd that is why you should be careful what you click on!')
 
-        elif user_input == 'settings':
-            with open('config.json', 'r') as jsonConfig:
-                config = json.dumps(json.load(jsonConfig), sort_keys=True, indent=4, separators=(',', ': '))
-            print(Fore.YELLOW+config)
-            
-        elif user_input == 'log':
-            print(Fore.BLUE+'Printing log.log ...')
-            f = open('main.log', 'r')
-            file_contents = f.read()
-            print (file_contents)
-            f.close()
+        elif user_input == 'stats':
+            # creating a DataFrame
+            dict = {'Stats':['Connected to Internet', 'Balance'],
+                    'Result':[
+                        check_internet_connection(),
+                        '${}'.format(mod_config('view','balance',''))
+                    ]}
+            df = pd.DataFrame(dict)
+            # displaying the DataFrame
+            print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
 
-        elif user_input == 'log -clean':
-            print('cleaning log...')
-            myText = open(r'main.log','w')
-            myText.write('<<< start of log >>>')
-            myText.close()
-            print('log cleaned!')
+        # Display all system information
+        elif user_input == 'system':
+            print(Fore.BLUE+'Opening python file for "system information"...')
+            os.system('python info/system.py')
+            import webbrowser
+            webbrowser.open('https://github.com/shannon-nz')
 
-        elif user_input == 'log -disable':
-            mod_config('mod','logOption','disabled')
-
-        elif user_input == 'log -enable':
-            mod_config('mod','logOption','enabled')
-
-        elif user_input == 'restart':
-            os.system('python app.py')
-            exit()
-
-        elif user_input == 'quit':
-            print(Fore.RED+'quiting the application...')
-            exit()
-            print(Fore.RED+'Failed to quit the application, please try again')
-
-        elif user_input == 'help':
-            print(Fore.BLACK+'   clear           '+Fore.BLUE+'clear the terminal (action)')
-            print(Fore.BLACK+'   lu              '+Fore.BLUE+'lucky unicorn (game)')
-            print(Fore.BLACK+'   rps             '+Fore.BLUE+'rock paper scissors (game)')
-            print(Fore.BLACK+'   hl              '+Fore.BLUE+'higher/lower (game)')
-            print(Fore.BLACK+'   stats           '+Fore.BLUE+'View your stats')
-            print(Fore.BLACK+'   system          '+Fore.BLUE+'get your system information (info)')
-            print(Fore.BLACK+'   profile         '+Fore.BLUE+'open my profile website (website)')
-            print(Fore.BLACK+'   doc             '+Fore.BLUE+'open python games documentation (website)')
-            print(Fore.BLACK+'   spaz            '+Fore.BLUE+'SPAZ YOUR SCREEN for 7 seconds (fun)')
-            print(Fore.BLACK+'   settings        '+Fore.BLUE+'print settings in JSON format (info)')
-            print(Fore.BLACK+'   log             '+Fore.BLUE+'view all past logs (info)')
-            print(Fore.BLACK+'   log -clean      '+Fore.BLUE+'view all past logs (info)')
-            print(Fore.BLACK+'   log -disable    '+Fore.BLUE+'disable logs (action)')
-            print(Fore.BLACK+'   log -enable     '+Fore.BLUE+'enable logs (info)')
-            print(Fore.BLACK+'   restart         '+Fore.BLUE+'restart the application (action)')
-            print(Fore.BLACK+'   quit            '+Fore.BLUE+'quit the entire program (action)')
-            print(Fore.BLACK+'   help            '+Fore.BLUE+'display command details (help)')
-            print(Fore.BLACK+'   help -des       '+Fore.BLUE+'display command details and an longer description (help)')
-
-        elif user_input == 'help -des':
-            print('help -des coming soon')
-
+        # This option is hidden because it deletes all modules, which would require the user to install them again
         elif user_input == 'uneverything':
-            # This option is hidden because it deletes all modules, which would require the user to install them again
             import pip
             pip.main(['uninstall', 'colorama'])
             pip.main(['uninstall', 'pynput'])
             pip.main(['uninstall', 'keyboard'])
 
-        elif user_input == 'reset':
-            print('reset here')
+        elif user_input == 'hello':
+            print('Oh, hello! What would you like me to do for you today?')
 
         else:
-            print(Fore.RED+'{} is an invalid command'.format(user_input))
+            print('Sorry, "{}" '.format(user_input)+Fore.RED+'is an invalid command')
             add_log('{} is an invalid command'.format(user_input))
 command_line()
