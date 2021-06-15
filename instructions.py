ef808436e9d164a5303d1449f74e23e26e356b37
Shannon A <slekupvimplyrataqq@protonmail.com>
2021-06-15 16:13:24 +1200

    move files

--- instructions.py
+++
@@ -1,2 +0,0 @@
-import webbrowser
-webbrowser.open('https://github.com/shannon-nz/python-games#commands')
---
+++ setup/update_pip.py
@@ -0,0 +1,4 @@
+import os
+print('Updating pip...')
+os.system('python -m pip install -U pip')
+print('program finished...')
