f5009481b41b7c751a4ab7bc206c114fa24d207c
Shannon A <slekupvimplyrataqq@protonmail.com>
2021-06-23 07:56:15 +1200

    to vid22 for RPS

--- main/games/rock_paper_scissors/Archive/01_RPS_Base_Component.py
+++ main/games/rock_paper_scissors/Archive/01_RPS_Base_Component.py
@@ -10,39 +10,11 @@ rps_list = ['rock', 'paper', 'scissors', 'quit']
 
 # Ask user if they have played before.
 # If 'yes', show the instructions
-def choice_checker(question, valid_list, error):
-    valid = False
-    while not valid:
-        # Ask use for choice (and put choice in lowercase)
-        response = input(question).lower()
-
-        # iterate through list and if response is an item in the list (or the first letter of an item), the full item name is returned
-
-        for item in valid_list:
-            if response == item[0] or response == item:
-                return response
-
-        # output error if item is not list
-        if response == 'r' or response == 'rock':
-            return response
-        elif response == 'p' or response == 'paper':
-            return response
-        elif response == 's' or response == 'scissors':
-            return response
-
-        elif response == 'quit':
-            return response
-        else:
-            print(error)
 
 
+# ask user for # of rounds, then loop...
 
+# Ask user if they want to see their game history
+#if yes, show the game history
 
-
-# Loop for testing purposes
-user_choice = ''
-while user_choice != 'quit':
-    # Ask user for choice and check it's valid
-    user_choice = choice_checker('Choose rock/paper/scissors (r/p/s): ', rps_list, 'Please choose from rock/paper/scissors or type "quit" to quit')
-    # print out choice for comparison purposes
-    print('You chose '+user_choice)
+# Show game statistics
