5cfeb01299e0d1e58c3393f5f4d2cdd81c532a59
Shannon A <slekupvimplyrataqq@protonmail.com>
2021-07-04 16:35:22 +1200

    add choice_checker function to base component

--- main/games/rock_paper_scissors/Archive/01_RPS_Base_Component.py
+++ main/games/rock_paper_scissors/Archive/01_RPS_Base_Component.py
@@ -1,6 +1,30 @@
 import random
 
 # Functions go here
+def choice_checker(question, valid_list, error):
+    valid = False
+    while not valid:
+        # Ask use for choice (and put choice in lowercase)
+        response = input(question).lower()
+
+        # iterate through list and if response is an item in the list (or the first letter of an item), the full item name is returned
+
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return response
+
+        # output error if item is not list
+        if response == 'r' or response == 'rock':
+            return response
+        elif response == 'p' or response == 'paper':
+            return response
+        elif response == 's' or response == 'scissors':
+            return response
+
+        elif response == 'quit':
+            return response
+        else:
+            print(error)
 
 # Main routine goes here
 
